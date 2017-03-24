89b3d8fdf9bbcd7f9d0558f7b2fad013e4a2332f
Edmund Johnson <edmund@jumpingmouse.uk>
2017-03-24 12:44:08 +0530

    Step 1: Add Java library: jokeprovider.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,10 +2,13 @@ package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.udacity.gradle.jokeprovider.JokeProvider;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        String joke = (new JokeProvider()).getJoke();
+        Toast toast = Toast.makeText(this, joke, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 
 
---
+++ jokeprovider/src/main/java/com/udacity/gradle/jokeprovider/JokeProvider.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.jokeprovider;
+
+public class JokeProvider {
+
+    public String getJoke() {
+        return "Waiter, this coffee tastes like mud!\n\nIt was ground this morning!";
+    }
+
+}
